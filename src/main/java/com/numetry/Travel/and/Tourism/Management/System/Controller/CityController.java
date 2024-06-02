e5a56393c1d08e0435ebfe67a6b131e4aa0ffc85
israr husain <54683061+israrhusain5892@users.noreply.github.com>
2024-06-02 21:10:28 +0530

    Update CityController.java

--- src/main/java/com/numetry/Travel/and/Tourism/Management/System/Controller/CityController.java
+++ src/main/java/com/numetry/Travel/and/Tourism/Management/System/Controller/CityController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/city")
+@RequestMapping("/public/city")
 @CrossOrigin
 public class CityController {
 
